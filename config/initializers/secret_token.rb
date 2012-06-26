98949cbbeaef003024637b2ddd02f70bc45d2b2f
vuille_c <charles.vuillecard@epitech.eu>
2012-06-26 16:14:02 +0200

    starting_dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'bc46f1904dcd2eedbae7009e6ec7a642e292ef47712a870149ea0a5b26ad12ec678e62eb1d6783408dd9f1f4126ecf315a6a8330aa6169e51188b128a78b7a89'
